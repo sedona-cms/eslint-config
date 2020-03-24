889cbab21badaaf84860c749ce56fa97ed930058
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-03-24 20:19:13 +0300

    chore: init commit

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:unicorn/recommended",
+    "plugin:promise/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["@typescript-eslint", "unicorn"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "@typescript-eslint/explicit-function-return-type": [
+      "warn",
+      {
+        allowExpressions: true,
+      },
+    ],
+    "@typescript-eslint/no-parameter-properties": [
+      "error",
+      { allows: ["readonly", "private readonly"] },
+    ],
+    "unicorn/filename-case": [
+      "error",
+      { cases: { pascalCase: true, kebabCase: true } },
+    ],
+  },
+};
