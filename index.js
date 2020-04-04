12c9ddedf08819ef5bccd17a38c30b8ce7fc7f38
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-04-04 11:36:00 +0300

    wip: add `prop`, `props`, `req`, `res` to ignore for replacement

--- index.js
+++ index.js
@@ -5,38 +5,46 @@ module.exports = {
     es6: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:unicorn/recommended",
-    "plugin:promise/recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: "module",
-    parser: "@typescript-eslint/parser",
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
   },
-  plugins: ["@typescript-eslint", "unicorn"],
+  plugins: ['@typescript-eslint', 'unicorn'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "@typescript-eslint/explicit-function-return-type": [
-      "warn",
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
       {
         allowExpressions: true,
       },
     ],
-    "@typescript-eslint/no-parameter-properties": [
-      "error",
-      { allows: ["readonly", "private readonly"] },
+    '@typescript-eslint/no-parameter-properties': [
+      'error',
+      { allows: ['readonly', 'private readonly'] },
     ],
-    "unicorn/filename-case": [
-      "error",
-      { cases: { pascalCase: true, kebabCase: true } },
+    'unicorn/filename-case': ['error', { cases: { pascalCase: true, kebabCase: true } }],
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          prop: false,
+          props: false,
+          res: false,
+          req: false,
+        },
+      },
     ],
   },
-};
+}
